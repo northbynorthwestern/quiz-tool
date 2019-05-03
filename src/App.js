91783e03288bb9c312dda13f58bdb87400d3c917
sujunglee <sujunglee2020@u.northwestern.edu>
2019-05-03 17:08:33 -0500

    Renders questions with a varying number of answer options so it can accommodate T/F and multiple-choice

--- src/App.js
+++ src/App.js
@@ -14,19 +14,44 @@ class App extends Component {
     Tabletop.init({
       // Paste in the key of your published Google spreadsheet in here
       key: "1rrBdYaTg9FiTO8I3PJFhZeChD8WZOs7K4zqWfkLXJOY",
-      callback: data => {
-        // Update the app's state with the info from the spreadsheet
-        this.setState({
-          data: data
-        });
-      },
+      callback: this.parseData,
       simpleSheet: true
     });
   }
 
+  parseData = data => {
+    var num_q = data.length; // total # of questions in quiz
+
+    // The (maximum) number of options available
+    // for each quiz question
+    var num_opt = Object.keys(data[0]).length - 2;
+
+    var parsed_data = []; // Reformatted data
+
+    for (var i = 0; i < num_q; i++) {
+      var curr_q = data[i];
+      var new_q = [curr_q["Question"]];
+      var curr_opt = [];
+
+      for (var j = 1; j <= num_opt; j++) {
+        var opt_name = "Option" + String(j);
+        if (curr_q[opt_name] !== "") {
+          curr_opt.push(curr_q[opt_name]);
+        }
+      }
+      new_q.push(curr_opt);
+      parsed_data.push(new_q);
+    }
+
+    // Update the app's state with the info from the spreadsheet
+    this.setState({
+      data: parsed_data
+    });
+  };
+
   render() {
     const { data } = this.state;
-    console.log("Updated state: ", this.state);
+    console.log("Updated spreadsheet data: ", data);
 
     return (
       <div className="App">
@@ -43,10 +68,17 @@ class App extends Component {
 
         <div>
           <h1>The Quiz</h1>
-          {data.map(row => {
+          {data.map(question => {
             return (
               <div>
-                <p>{row.Question}</p>
+                <p>{question[0]}</p>
+                {question[1].map(option => {
+                  return (
+                    <div>
+                      <p>{option}</p>
+                    </div>
+                  );
+                })}
               </div>
             );
           })}
